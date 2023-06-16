41dc3bc3b3d6df61b3480ac6ec501d7de301aad3
saket <saketbharti201721@gmail.com>
2023-06-16 15:07:17 +0530

    emplee class make

--- payroll_management.cpp
+++ payroll_management.cpp
@@ -22,7 +22,32 @@ class Menu
 
 };
 
+class Employee 
+{
+    private:
+    void add_record(int , char[], char [], char[], int ,int, int, char [], char, char, char, float, float);
+    void modify_record(int, char[], char[], char[], char[], char, char, char, float, float);
+    void delete_record(int);
+    void last_code();
+    void code_found(int);
+    void record_no(int);
+    void display_record(int);
+    int valid_date(int, int , int);
+    
+    int dd, mm, yyyy, code;
+    char name[26], address[31], phone[10], design[16];
+    char grade, house, convence;
+    float loan, basic;
 
+    public :
+    void new_employee();
+    void modification();
+    void deletion();
+    void display();
+    void list();
+    void salary_slip();
+
+};
 
 
 int main()
