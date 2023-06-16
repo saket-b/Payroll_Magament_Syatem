5b545a36022bd333adf3648de5e8bb6a063fa7f2
saket <saketbharti201721@gmail.com>
2023-06-16 14:58:55 +0530

    make line class

---
+++ payroll_management.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Lines
+{   
+    public:
+    void line_hor(int, int, int , char);
+    void line_ver(int, int, int , char);
+    void box(int, int , int , int , char);
+    void clear_up();
+    void clear_down();
+
+};
+
+
+int main()
+{
+
+
+    return (0);
+}
